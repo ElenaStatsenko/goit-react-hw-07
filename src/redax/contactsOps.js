171b27746910651b77fe08f04bee9a90896de9b3
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2024-09-11 21:55:40 +0300

    сделала try catch

--- src/redax/contactsOps.js
+++ src/redax/contactsOps.js
@@ -2,8 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://66e04b982fb67ac16f291bbd.mockapi.io";
-export const fetchTasks = createAsyncThunk("items/fetchAll", async () => {
+export const fetchTasks = createAsyncThunk("items/fetchAll", async (_, thunkAPI) => {
+  try {
     const response = await axios.get("/items");
     console.log(response.data)
     return response.data;
+  } catch (error) {
+   return thunkAPI.rejectWithValue(5)
+   
+  }
+  
   });
